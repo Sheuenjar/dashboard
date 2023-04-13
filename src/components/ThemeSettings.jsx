89ed889f213fa0da408ac7415e6970ea2069356d
sheuen <sheuenjaramillo@gmail.com>
2023-04-13 15:27:38 -0300

    Apps ended and init with ThemeSettings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+
+
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+/* import {  } */
+
 
 const ThemeSettings = () => {
   return (
     <div>ThemeSettings</div>
   )
 }
+
 
 export default ThemeSettings
