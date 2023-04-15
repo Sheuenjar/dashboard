f1d95c9f76f050392b77a524f6bff8eca8c80153
sheuen <sheuenjaramillo@gmail.com>
2023-04-15 17:25:46 -0300

    ChartPages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
         <TooltipComponent content='Profile' position='BottomCenter'>
           <div 
             className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
-            onClick={() => handleClick('userProfile')}
+            onClick={() => setIsClicked('userProfile')}
           >
             <img className='rounded-full w-8 h-8' src={avatar}/>
             <p>
